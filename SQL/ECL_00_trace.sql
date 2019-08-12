ebc3f598d75c13f13c0a1f757e21fb4da1da04ee
gregbodet <37414914+gregbodet@users.noreply.github.com>
2019-08-12 10:01:20 +0200

    Create ECL_00_trace.sql

---
+++ SQL/ECL_00_trace.sql
@@ -0,0 +1,18 @@
+/*ECL V1.0*/
+/*Creation du fichier trace qui permet de suivre l'évolution du code*/
+/* ECL_10_trace.sql */
+/*PostGIS*/
+
+/* Propriétaire : GeoCompiegnois - http://geo.compiegnois.fr/ */
+/* Auteur : Thibaut Guill */
+
+/*  
+  dépendances : voir script d'initialisation des dépendances init_bd_pei_dependencies.sql
+  
+  Liste des dépendances :
+  schéma          | table                 | description                                                   | usage
+*/
+
+/*
+#################################################################### SUIVI CODE SQL ####################################################################
+2019-08-12 : TG / initialisation du code
