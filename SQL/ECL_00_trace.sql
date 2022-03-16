6cfa54397c7eec968affd4329dae84284aeebc09
gregbodet <37414914+gregbodet@users.noreply.github.com>
2022-03-16 12:27:30 +0100

    Update ECL_00_trace.sql

--- SQL/ECL_00_trace.sql
+++ SQL/ECL_00_trace.sql
@@ -44,3 +44,4 @@
                   Intégration d'un attribut du stock du nombre de lampe dans la table des modèles de lampes (à venir fonctionnel de décompte par rapport aux interventions de changement de lampes)
 2021-07-01 : GB / Intégration fonction trigger pour automatiser le stock en fonction des interventions de changement de lampe
 2021-08-19 : GB : Correction sur indicateur linéaire cable, affectation bonne variable pour la saisie par le service et non sur la zone de gestion ville
+2022-03-16 : GB : intégration attribut sur PI si alimenté par énergie solaire + possibilté de modifié les caractéristiques des PI
